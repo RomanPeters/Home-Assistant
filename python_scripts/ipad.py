cccdad50d48efa1c2efd3f7a68a897975e9a2df6
Roman Peters <mail@romanpeters.nl>
2019-02-09 13:41:19 +0100

    Cleanup

--- python_scripts/ipad.py
+++ python_scripts/ipad.py
@@ -1,14 +1,18 @@
+import subprocess
+
+host = "root@10.10.10.3"
 
 command = data.get("command")
 arguments = data.get("data")
 if command == 'say':
-    subprocess.check_output('ssh ipad say {}'.format(arguments), shell=True)
+    x = subprocess.check_output(f'ssh -i /data/.ssh/id_rsa -o StrictHostKeyChecking=no {host} say {arguments}', shell=True)
+    logger.warning(x)
 elif command == 'siri':
-    subprocess.check_output("ssh ipad say hey siri, {}".format(arguments), shell=True)
+    subprocess.check_output(f"ssh {host} say hey siri, {arguments}", shell=True)
 elif command == 'remote':
-    subprocess.check_output("ssh ipad activator send com.apple.mobilesafari", shell=True)
+    subprocess.check_output(f"ssh {host} activator send com.apple.mobilesafari", shell=True)
 elif command == 'sleep':
-    subprocess.check_output("ssh ipad activator send libactivator.system.sleepbutton", shell=True)
+    subprocess.check_output(f"ssh {host} activator send libactivator.system.sleepbutton", shell=True)
 elif command == 'ringtone':
     # News Flash, New Mail, Update, Ladder, Calypso, Minuet, Descent, Suspense, Bloom, Glass, Tri-tone, Choo Choo, Chime, Electronic
     # Bell, Telegraph, Anticipate, Sherwood Forest, Noir, Tiptoes, Typewriters, Calendar Alert, Sent Mail, Fanfare, Sent Tweet, Horn
@@ -18,7 +22,7 @@ elif command == 'ringtone':
         arguments = 'Tri-tone'
     else:
         arguments = arguments.title()
-    subprocess.check_output("ssh ipad activator send libactivator.ringtone.system:{}".format(arguments), shell=True)
+    subprocess.check_output(f"ssh {host} activator send libactivator.ringtone.system:{arguments}", shell=True)
 elif command == "test":
-    subprocess.Popen(["ssh", "-i /data/.ssh/id_rsa root@ipad", "say hello world"], shell=True) #, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    subprocess.Popen([f"ssh -i /data/.ssh/id_rsa -o StrictHostKeyChecking=no {host} say 'hello world'"], shell=True) #, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     logger.warning("Yes")
